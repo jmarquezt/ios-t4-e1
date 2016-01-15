7e5924cfdde7c7d0f2110ee0d9cd3760fe69b936
Jorge Marquez Torres <jmarquez@jorges-mac.local>
2016-01-15 12:46:16 +0100

    Initial Commit

---
+++ T4E1/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  T4E1
+//
+//  Created by Jorge Marquez Torres on 15/1/16.
+//  Copyright © 2016 jmarquez. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
